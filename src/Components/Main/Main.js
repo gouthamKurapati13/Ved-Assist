a6a8d423a85a08d04f4d0f7da7767bf2fb94dea1
Goutham Kurapati <gouthamkurapati@gmail.com>
2023-12-20 12:58:12 +0530

    Added About

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,21 +1,13 @@
 import Navbar from "./Navbar/Navbar";
 import Home from "./Home/Home";
-<<<<<<< HEAD
-import About from "./About/About";
-=======
 import Team from "./Team/Team";
->>>>>>> 87a0db3076c34a517f697050c07c4beb40a994b5
 
 function Main() {
     return (
         <>
             <Navbar />
             <Home />
-<<<<<<< HEAD
-            <About />
-=======
             <Team />
->>>>>>> 87a0db3076c34a517f697050c07c4beb40a994b5
         </>
     );
 }
