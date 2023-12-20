2232599ecf1a794c960974c2ec0b5893cb82aeef
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-12-20 12:02:10 +0530

    commit

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -2,8 +2,8 @@ import "./Home.css";
 function Home() {
     return (
         <>
-            <section className="container-fluid px-0">
-      <div className="row align-items-center">
+            <section id="home">
+      {/* <div className="row align-items-center">
         <div className="col-lg-3"></div>
         <div id="headingGroup" className="col-lg-6 text-center mt-5">
           <h1>Vedassist</h1>
@@ -12,7 +12,11 @@ function Home() {
           <button id="button3"><a href="">Get Started</a></button>
         </div>
         <div className="col-lg-3"></div>
-      </div>
+      </div> */}
+<h1>Vedassist</h1>
+          <h2><b>Unlock the power of Ayurveda</b></h2>
+          <h3>Predict your perfect remedy, revolutionize your well-being</h3>
+          <button id="button3"><a href="">Get Started</a></button>
     </section>
         </>
     );
