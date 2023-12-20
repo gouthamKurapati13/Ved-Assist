2fb4b15b15da8174f37f3f86833d0ec887f26a72
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-12-20 11:50:02 +0530

    First part

--- src/Components/Main/Home/Home.js
+++ src/Components/Main/Home/Home.js
@@ -1,7 +1,19 @@
+import "./Home.css";
 function Home() {
     return (
         <>
-            
+            <section className="container-fluid px-0">
+      <div className="row align-items-center">
+        <div className="col-lg-3"></div>
+        <div id="headingGroup" className="col-lg-6 text-center mt-5">
+          <h1>Vedassist</h1>
+          <h2><b>Unlock the power of Ayurveda</b></h2>
+          <h3>Predict your perfect remedy, revolutionize your well-being</h3>
+          <button id="button3"><a href="">Get Started</a></button>
+        </div>
+        <div className="col-lg-3"></div>
+      </div>
+    </section>
         </>
     );
 
