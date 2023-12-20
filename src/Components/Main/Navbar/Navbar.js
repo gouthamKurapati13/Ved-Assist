85b73ca271994ce80705021237774d671d9cdd95
Goutham Kurapati <gouthamkurapati@gmail.com>
2023-12-20 12:02:33 +0530

    Made the navbar responsive

--- src/Components/Main/Navbar/Navbar.js
+++ src/Components/Main/Navbar/Navbar.js
@@ -4,33 +4,41 @@ import "./Navbar.css";
 
 function Navbar() {
     const [showNavbar, setShowNavbar] = useState(false);
+    const [menuIcon, setMenuIcon] = useState("fa fa-bars");
     const handleShowNavbar = () => {
-        const handleShowNavbar = () => {
-            setShowNavbar(!showNavbar);
-        };
+        setShowNavbar(!showNavbar);
+        if (menuIcon==="fa fa-bars")
+            setMenuIcon("fa fa-times");
+        else
+            setMenuIcon("fa fa-bars");
     }
 
     return (
         <>
             <nav>
-                <div class="logo">
+                <div className="logo">
                     <a href="#">
                         <img src={Logo} alt="Logo" />
                     </a>
                 </div>
-                <ul class="nav-links">
-                    <li><a href="#" class="active-link">Home</a></li>
+                <div className="menu-bar" onClick={handleShowNavbar}>
+                    <i className={menuIcon}></i>
+                </div>
+                <ul className={(showNavbar)?'nav-links active':'nav-links'}>
+                    <li><a href="#" className="active-link">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Team</a></li>
+                    <div className="mob-btn-grp">
+                        <button>Predict</button>
+                        <button>Shop</button>
+                    </div>
                 </ul>
-                <div class="btn-grp">
+                <div className="btn-grp">
                     <button>Predict</button>
                     <button>Shop</button>
                 </div>
 
-                <div className="menu-bar" onClick={handleShowNavbar}>
-                    <i></i>
-                </div>
+                
             </nav>
         </>
     );
