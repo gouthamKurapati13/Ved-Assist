2609a036ef153a26952a629329b0db5185983730
Gokul <114865522+GokulAnithaNandakumar@users.noreply.github.com>
2023-12-22 16:00:08 +0530

    Shop home done

---
+++ src/Components/Main/ShopHeader/ShopHeader.js
@@ -0,0 +1,18 @@
+import "./ShopHeader.css";
+function ShopHeader() {
+    return (
+        <>
+        <button id="button1">History</button>   
+
+          <section id="shopheader">
+            <h1>Shop</h1>
+            <h2><b>One stop for all your Ayurvedic medicines</b></h2>
+            <h3>Make sure you read the description before you buy</h3>
+
+          </section>
+        </>
+    );
+
+}
+
+export default ShopHeader;
