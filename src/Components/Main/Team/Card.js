5a0c939237ae841387a19f85ee0b847577c8046c
Goutham Kurapati <gouthamkurapati@gmail.com>
2023-12-27 21:59:54 +0530

    Updated team

--- src/Components/Main/Team/Card.js
+++ src/Components/Main/Team/Card.js
@@ -1,6 +1,6 @@
 import "./Card.css"
 
-function Card({Devimg}) {
+function Card({Devimg, name, role, github, linkedin, mail}) {
     return (
         <>
             <div className="card">                
@@ -8,13 +8,13 @@ function Card({Devimg}) {
                     <img src={Devimg} alt="" />
                 </div>
                 <div id="dev-div">
-                    <h3>Lorem</h3>
-                    <h4>Developer</h4>
+                    <h3>{name}</h3>
+                    <h4>{role}</h4>
                     <div className="socials">
                         <ul>
-                            <li><a href=""><i className="fa fa-github"></i></a></li>
-                            <li><a href=""><i className="fa fa-linkedin"></i></a></li>
-                            <li><a href=""><i className="fa fa-envelope-o"></i></a></li>
+                            <li><a href={github} target="_blank"><i className="fa fa-github"></i></a></li>
+                            <li><a href={linkedin} target="_blank"><i className="fa fa-linkedin"></i></a></li>
+                            <li><a href={mail} target="_blank"><i className="fa fa-envelope-o"></i></a></li>
                         </ul>
                     </div>
                 </div>
--- src/Components/Main/Team/Team.js
+++ src/Components/Main/Team/Team.js
@@ -1,21 +1,21 @@
 import "./Team.css";
 import Devimg from "./devlogo.png";
+import Amrit from "./Amrit_Icon.jpg";
+import Goutham from "./Goutham_Icon.jpg";
+import Vijay from "./Vijay_Icon.jpg";
+import Gokul from "./Gokul_Icon.jpg";
 import Card from "./Card";
 
 function Team() {
     return (
         <>
-      
-      
-
-
     <section id="team">
       <h1 className="main-heading">Our Team</h1>
       <div className="team-row">
-        <Card Devimg={Devimg} />
-        <Card Devimg={Devimg} />
-        <Card Devimg={Devimg} />
-        <Card Devimg={Devimg} />
+        <Card Devimg={Vijay} name="Vijay Varadarajan" role="Backend Developer" github="https://github.com/vijay-varadarajan" linkedin="https://www.linkedin.com/in/vijay-varadarajan-630597200/" mail="mailto:vijayvaradarajan84@gmail.com"/>
+        <Card Devimg={Amrit} name="Amrit Sundarka" role="Backend Developer" github="https://github.com/Amrit02102004" linkedin="https://www.linkedin.com/in/amrit-sundarka-6a9b89249/" mail="mailto:amritsundarka2004@gmail.com"/>
+        <Card Devimg={Goutham} name="Goutham Kurapati" role="Frontend Developer" github="https://github.com/gouthamKurapati13/" linkedin="https://www.linkedin.com/in/goutham-kurapati" mail="mailto:gouthamkurapati@gmail.com" />
+        <Card Devimg={Gokul} name="Gokul Nandakumar" role="Frontend Developer" github="https://github.com/GokulAnithaNandakumar" linkedin="http://www.linkedin.com/in/gokulnandakumar" mail="mailto:gokul.nandakumar04@gmail.com" />
       </div>
     </section>
 
